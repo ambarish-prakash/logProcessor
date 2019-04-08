30b1f02d52f7e0ce67d1b7f9d7e7e61631140b2c
ambarish004 <ambarish004@gmail.com>
2019-04-09 01:06:08 +0800

    Allow CORS

--- src/main/java/com/ambarish/logs/logprocessor/rest/LPRestController.java
+++ src/main/java/com/ambarish/logs/logprocessor/rest/LPRestController.java
@@ -2,16 +2,23 @@ package com.ambarish.logs.logprocessor.rest;
 
 import com.ambarish.logs.logprocessor.service.LogInsertionService;
 import com.ambarish.logs.logprocessor.service.StatusRetrivalService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /***
  * Rest controller for the endpoints
  */
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class LPRestController {
 
     Logger logger = LoggerFactory.getLogger(LPRestController.class);
@@ -30,9 +37,10 @@ public class LPRestController {
      * @return status of object at given timeframe
      */
     @RequestMapping( method = RequestMethod.GET, value = "/status/{oType}/{oId}/{ts}")
-    public String getStatus(@PathVariable("oType") String oType, @PathVariable("oId") String oId, @PathVariable("ts") long ts) {
+    public ResponseEntity<Map<String,String>> getStatus(@PathVariable("oType") String oType, @PathVariable("oId") String oId, @PathVariable("ts") long ts) {
         logger.info("Received status request for type: " + oType + ", id: " + oId + ", ts: " + ts );
-        return statusRetrivalService.getStatusForOrder(oType,oId,ts);
+        Map<String,String> res =  new HashMap<String, String>(1){{put("result", statusRetrivalService.getStatusForOrder(oType,oId,ts));}};
+        return new ResponseEntity<>(res, HttpStatus.ACCEPTED);
     }
 
     /***
